15e74b9c2f6c77997a6615fd34e14079baa33eb9
levara <levara@levara.xyz>
2023-02-06 21:40:14 +0100

    Add saving a new job in views using a JobForm

--- translateapp/app/views.py
+++ translateapp/app/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.urls import reverse
 from .models import Job, Bid, Dispute, Message, Rating
 from .forms import JobForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -17,13 +18,38 @@ def jobs_index(request):
     }
     return render(request, 'app/jobs_index.html', context)
 
+@login_required
 def new_job(request):
-    form = JobForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'app/jobs_new.html', context)
-    pass
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = JobForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # save the form data to model and persist to db
+            job = form.save(commit=False)
+            # set the user to the current user
+            job.user = request.user
+            # set the status to available using the enum defined in the model class
+            job.status = Job.Status.AVAILABLE
+            # Persist the job to the db
+            job.save()
+
+            # redirect to job listing page:
+            return HttpResponseRedirect(reverse('app:jobs'))
+        else: 
+            # if form is not valid, render the form with error messages
+            # and the data the user entered. The error messages are contained
+            # in the form.errors dictionary and can be accessed in the template.
+            return render(request, 'app/new_job.html', {'form': form})
+    else:
+        # if a GET (or any other method) we'll create a blank form
+        # and render it in the template like usual
+        form = JobForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'app/jobs_new.html', context)
 
 def job_detail(request, job_id):
     pass
